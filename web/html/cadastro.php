7604be483e304cf2701332b6b99a1797fecb3091
kbz08 <kbarboza075@gmail.com>
2025-05-20 11:58:26 -0300

    pagina signup

--- web/html/cadastro.php
+++ web/html/cadastro.php
@@ -11,80 +11,78 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400..800&display=swap" rel="stylesheet">
-        <title>Document</title>
-    </head>
-
-    <body>
-
-        <header>
-            <div class="interface">
-                <div class="logo">
-                    <a href="#">
-                        <img src="../imagens/logo.png">
-                    </a>
-                </div>
+  <title>Sign up</title>
+</head>
+<body>
+  <header>
+    <div class="interface">
+      <div class="logo">
+        <a href="#">
+          <img src="../imagens/logo.png">
+        </a>
+      </div>
+    </div>
+  </header>
+ 
+  <div class="main-login">
+    <div class="left-login">
+      <div class="btns-logins">
+        <h1>SEJA BEM-VINDO!<br></h1>
+        <a href="#" class="btn-email">
+          <button type="button">Entre com o e-mail</button>
+        </a>
+        <a href="#" class="btn-gmail">
+          <button type="button">Entre com o gmail</button>
+        </a>
+        <a href="#" class="btn-numerodetel">
+          <button type="button">Entre com o Nº de telefone</button>
+        </a>
+      </div>
+    </div>
+ 
+    <div class="right-login">
+      <div class="card-login">
+        <h1>Sign up</h1>
+        <form method="post" action="../back-end/php/signup.php">
+          <div class="textfield">
+            <label for="nome">Nome Completo: </label>
+            <input type="text" name="usuario" placeholder="Insira seu nome">
+          </div>
+ 
+          <div class="textfield">
+            <label for="e-mail">Endereço de E-mail: </label>
+            <input type="text" name="email" placeholder="Zamboni@gmail.com">
+          </div>
+ 
+          <!-- Campos lado a lado -->
+          <div class="textfield-row">
+            <div class="textfield">
+              <label for="senha">Senha:</label>
+              <input type="password" name="senha" placeholder="Senha">
             </div>
-        </header>
-        <div class="main-login">
-            <div class="left-login">
-                <div class="btns-logins">
-                    <h1>SEJA BEM-VINDO!<br></h1>
-                    <a href="#" class="btn-email">
-                        <button type="button">Entre com o e-mail</button>
-                    </a>
-                    <a href="#" class="btn-gmail">
-                        <button type="button">Entre com o gmail</button>
-                    </a>
-                    <a href="#" class="btn-numerodetel">
-                        <button type="button">Entre com o Nº de telefone</button>
-                    </a>
-                </div>
+            <div class="textfield">
+              <label for="confsenha">Confirme sua Senha:</label>
+              <input type="password" name="confsenha" placeholder="Confirme a senha">
             </div>
-
-            <div class="right-login">
-                <div class="card-login">
-                    <h1>Sing up</h1>
-                    <form method="post" action="../back-end/php/signup.php">
-
-                        <div class="textfield">
-                            <label for="nome">Nome Completo: </label>
-                            <input type="text" name="usuario" placeholder="Insira seu nome">
-                        </div>
-
-                        <div class="textfield">
-                            <label for="e-mail">Endereço de E-mail: </label>
-                            <input type="text" name="email" placeholder="Zamboni@gmail.com">
-                        </div>
-
-                        <div class="textfield">
-                            <label for="senha">Senha:</label>
-                            <input type="password" name="senha" placeholder="Senha">
-                        </div>
-
-                        <div class="textfield">
-                            <label for="confsenha">Confirme sua Senha:</label>
-                            <input type="password" name="confsenha" placeholder="Confirme a senha">
-                        </div>
-                        <button class="btn-registrarse" type="submit">Registrar-se</button>
-                        <button class="btn-entrar">Entrar</button>
-                    </form>
-
-                   <div class="errologin"> 
-                        <?php if (!empty($erros)): ?>
-                            <div class="errolog">
-                                <?php foreach($erros as $erro): ?>
-                                    <p><?= htmlspecialchars($erro);  ?></p>
-                                <?php endforeach; ?>
-                            </div>    
-                        <?php endif; ?>;
-                    </div>
-                        
-                </div>
-            </div>
+          </div>
+ 
+          <button class="btn-registrarse" type="submit">Registrar-se</button>
+          <button class="btn-entrar">Entrar</button>
+        </form>
+ 
+        <div class="errologin">
+          <?php if (!empty($erros)): ?>
+          <div class="errolog">
+            <?php foreach($erros as $erro): ?>
+            <p><?= htmlspecialchars($erro); ?></p>
+            <?php endforeach; ?>
+          </div>
+          <?php endif; ?>
         </div>
-
-    </body>
-
-    <script src="../back-end/js/signup.js"></script>
-
+      </div>
+    </div>
+  </div>
+ 
+  <script src="../back-end/js/signup.js"></script>
+</body>
 </html>
