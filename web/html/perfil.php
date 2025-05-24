9a215903868c82fa154ab8b7bef849e61a03664c
biasous <bianca.sous17@gmail.com>
2025-05-23 21:37:39 -0300

    perfil responsivo+-

--- web/html/perfil.php
+++ web/html/perfil.php
@@ -33,7 +33,7 @@
 
             <form enctype="multipart/form-data" method="post" action="../back-end/php/signup2.php">
                 <div class="adc-img">
-                    <button><i class="bi bi-person-circle"><input type="file" id="envfto" name="envft" /></i></button>
+                    <button><i class="bi bi-person-circle"><br><input type="file" id="envfto" name="envft" /></i></button>
                 </div>
                 <div class="texto">
                     <div class="nomeexb">
