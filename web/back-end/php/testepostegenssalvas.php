3354092f43f509b2d41af736ca063c84bd4ae5f9
Bruna <livrosearquivos12@gmail.com>
2025-07-31 23:15:03 -0300

    banco atualizado
    Ele está atualizado, porém houve poucas modificações

--- web/back-end/php/testepostegenssalvas.php
+++ web/back-end/php/testepostegenssalvas.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "testepostegem")
+$conn = new mysqli("localhost", "root", "", "postagens")
 session_start();
 $_SESSION["usuario_id"] = 1;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $stmt = $con->prepare("INSERT into postagens (id_usuario, id, titulo, texto, data_post) VALUES (?, ?, ?, ?, ?, ?)");
  $stmt->execute();
 
-    echo "Coleção criada!";
+    echo "Sua postagem foi salva!";
 }
 ?>
 // essa é uma tentativa de salvas as postagens do usuário em uma lista, sendo puxada pela tabela postagens
