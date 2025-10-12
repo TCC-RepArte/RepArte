a384c9bd8ade6ee534c573f4a56576c3ca122b24
Bruna <livrosearquivos12@gmail.com>
2025-10-12 01:23:44 -0300

    teste de notificação

--- web/back-end/php/noti.php
+++ web/back-end/php/noti.php
@@ -1,65 +1,11 @@
 <?php
-include 'login.php';
-session_start ();
-
-global $con;
-
- function buscaUsuario(){
-
-    global $con;
-    // Utilizando da sessão do id para puxar outros dados do perfil
-    $id_usuario = $_SESSION['id'];
-
-    if(!isset($_SESSION['id'])){
-        header("Location: ../../web/html/login1.php");;
-        exit;
+class Connect extends PDO{
+    public function_construst (){
+        parent::_construst("msql:localhost;dbname=msp","root","",
+        array(PDO::MSQL_ATTR_INIT_COMMAND =>" SET NAPES utf8"));
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXECPTION);
+        $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
-     $id_usuario = $_SESSION['id'];
-
-    // Buscar dados do usuário
-    $sql = "SELECT * FROM usuarios WHERE id = ?";
-    $stmt = $con->prepare($sql);
-    $stmt->bind_param("i", $id_usuario);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
-    return $resultado->fetch_assoc();
 }
-    //// essa parte é para receber e puxar as informações do banco de dados 
-
-
-
-
-
-    // mostrando as notificações do usuario logado
-
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Notificações </title>
-     <a href="notificações.css"> Notificações </a>
-    </head>
-    <body>
-        <button id="ativarnoti" > Notificação </button>
-         <?php foreach ($notificacoes as $n): ?>
-      <p>📨 <?php echo htmlspecialchars($n['mensagem']); ?> (<?php echo $n['tipo']; ?>)</p>
-    <?php endforeach; ?>
-    </div>
-
-            <script> 
-            const idUsuario = 1; 
-            const socket = new WebSocket ('ws://localhost:8080/notificacoes');
-            socket.onopen = function () {
-                console.log ('Está conectado ao WS');
-                socket.send('Usuario:' + idUsuario) // informa qual é o usuário
-
-            };
-            socket_onmessage = function(event) {
-            const mensagem = event. data;
-            mostrarNoti(mensagem);
-            if (notification. permission === )
-            };
-    </body>
-    </html>
+$tbl_notificacoes = "notificacoes";
+$their ='id';
--- web/back-end/php/notificações.php
+++ web/back-end/php/notificações.php
@@ -1,5 +1,79 @@
+
 <?php
-include 'login.php';
+// fetch pagina
+require_once('noti.php');
+$db = new Connect;
+$data = [];
+global $tbl_notificacoes;
+if (isseed($_POST['key']) &&($_POST['key'] == '123')){
+    $notication = $db->prepare("Select * FROM $tbl_notificacoes order by id desc limit 10");
+$notication ->execute();
+$Nonotication->$noticationi=>rowCount();
+if($notication > 0)(
+
+
+$n_numero= $db=>prepare( "SELECT * FROM $tbl_notificacoes WERE noti_status= 'active' order by id desc");
+$n_numero
+$n_numeros = $n_numero =>rowCount();
+array_push($data,(object){
+    'total'=>$n_numeros,
+
+});
+while($notication =$notication => fetch(PDO::FETCH_ASSOC)){
+        $data[]= $notication;
+}
+};
+
+if (isseed($_POST['key']) &&($_POST['key'] == '1234')){
+
+}
+  
+
+
+/* include 'login.php';
+session_start ();
+
+global $con;
+
+ function buscaUsuario(){
+
+    global $con;
+    // Utilizando da sessão do id para puxar outros dados do perfil
+    $id_usuario = $_SESSION['id'];
+
+    if(!isset($_SESSION['id'
+<?php
+// fetch pagina
+require_once('noti.php');
+$db = new Connect;
+$data = [];
+global $tbl_notificacoes;
+if (isseed($_POST['key']) &&($_POST['key'] == '123')){
+    $notication = $db->prepare("Select * FROM $tbl_notificacoes order by id desc limit 10");
+$notication ->execute();
+$Nonotication->$noticationi=>rowCount();
+if($notication > 0)(
+
+
+$n_numero= $db=>prepare( "SELECT * FROM $tbl_notificacoes WERE noti_status= 'active' order by id desc");
+$n_numero
+$n_numeros = $n_numero =>rowCount();
+array_push($data,(object){
+    'total'=>$n_numeros,
+
+});
+while($notication =$notication => fetch(PDO::FETCH_ASSOC)){
+        $data[]= $notication;
+}
+};
+
+if (isseed($_POST['key']) &&($_POST['key'] == '1234')){
+    
+}
+  
+
+
+/* include 'login.php';
 session_start ();
 
 global $con;
@@ -23,25 +97,27 @@ global $con;
     $stmt->execute();
     $resultado = $stmt->get_result();
     return $resultado->fetch_assoc();
+    $tbl_notificacoes = "notificacoes"
 }
-    //// essa parte é para receber e puxar as informações do banco de dados 
-
-
-
-
+    //// essa parte é para receber e puxar as informações do banco de dados
 
     // mostrando as notificações do usuario logado
+require_once(noti.php)*/])){
+        header("Location: ../../web/html/login1.php");;
+        exit;
+    }
+     $id_usuario = $_SESSION['id'];
 
+    // Buscar dados do usuário
+    $sql = "SELECT * FROM usuarios WHERE id = ?";
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param("i", $id_usuario);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    return $resultado->fetch_assoc();
+    $tbl_notificacoes = "notificacoes"
+}
+    //// essa parte é para receber e puxar as informações do banco de dados
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Notificações </title>
-     <a href="notificações.css"> Notificações </a>
-    </head>
-    <body>
-        
-    </body>
-    </html>
+    // mostrando as notificações do usuario logado
+require_once(noti.php)*/
